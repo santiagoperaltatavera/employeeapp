b43f3c3b1fc9447ad56c0d8766d0c491ea3e9c09
santiagoperaltatavera <santiagoperalta95@hotmail.com>
2025-06-13 17:02:11 -0500

    Refactor validators: added implementation and updated interface

--- src/main/java/com/parameta/employeeapp/application/validators/EmployeeValidator.java
+++ src/main/java/com/parameta/employeeapp/application/validators/EmployeeValidator.java
@@ -1,31 +1,7 @@
 package com.parameta.employeeapp.application.validators;
 
 import com.parameta.employeeapp.infrastructure.rest.dto.EmployeeRequestDTOImpl;
-import com.parameta.employeeapp.shared.utils.DateUtils;
-import org.springframework.stereotype.Component;
 
-import java.time.LocalDate;
-
-@Component
-public class EmployeeValidator {
-
-    public void validate(EmployeeRequestDTOImpl dto) {
-        LocalDate today = LocalDate.now();
-
-        if (dto.getDateOfBirth().isAfter(today)) {
-            throw new IllegalArgumentException("Date of birth cannot be in the future");
-        }
-
-        if (dto.getHireDate().isBefore(dto.getDateOfBirth())) {
-            throw new IllegalArgumentException("Hire date cannot be before date of birth");
-        }
-
-        if (dto.getHireDate().isAfter(today)) {
-            throw new IllegalArgumentException("Hire date cannot be in the future");
-        }
-
-        if (DateUtils.calculatePeriod(dto.getDateOfBirth(), today).getYears() < 18) {
-            throw new IllegalArgumentException("Employee must be at least 18 years old");
-        }
-    }
+public interface EmployeeValidator<T> {
+    void validate(T dto);
 }
