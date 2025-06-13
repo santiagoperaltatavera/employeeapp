de950ad8aa8004500b153004028770b64e23c556
santiagoperaltatavera <santiagoperalta95@hotmail.com>
2025-06-13 14:47:26 -0500

    add general test

---
+++ src/test/java/com/parameta/employeeapp/EmployeeApplicationTests.java
@@ -0,0 +1,15 @@
+package com.parameta.employeeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class EmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
