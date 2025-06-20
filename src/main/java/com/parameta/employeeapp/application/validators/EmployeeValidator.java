8fae67d0af167b0f67de6123409595da8168dc73
santiagoperaltatavera <santiagoperalta95@hotmail.com>
2025-06-19 19:15:54 -0500

    Update EmployeeValidator and EmployeeValidatorImpl to include new validation rules.

--- src/main/java/com/parameta/employeeapp/application/validators/EmployeeValidator.java
+++ src/main/java/com/parameta/employeeapp/application/validators/EmployeeValidator.java
@@ -1,7 +1,5 @@
 package com.parameta.employeeapp.application.validators;
 
-import com.parameta.employeeapp.infrastructure.rest.dto.EmployeeRequestDTOImpl;
-
 public interface EmployeeValidator<T> {
     void validate(T dto);
 }
--- src/main/java/com/parameta/employeeapp/application/validators/EmployeeValidatorImpl.java
+++ src/main/java/com/parameta/employeeapp/application/validators/EmployeeValidatorImpl.java
@@ -1,30 +1,30 @@
 package com.parameta.employeeapp.application.validators;
 
-import com.parameta.employeeapp.infrastructure.rest.dto.EmployeeRequestDTOImpl;
+import com.parameta.employeeapp.infrastructure.rest.dto.EmployeeRequestDTO;
 import com.parameta.employeeapp.shared.utils.DateUtils;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
 
 @Component
-public class EmployeeValidatorImpl implements EmployeeValidator<EmployeeRequestDTOImpl> {
+public class EmployeeValidatorImpl implements EmployeeValidator<EmployeeRequestDTO> {
 
-    public void validate(EmployeeRequestDTOImpl dto) {
+    public void validate(EmployeeRequestDTO dto) {
         LocalDate today = LocalDate.now();
 
-        if (dto.getDateOfBirth().isAfter(today)) {
+        if (dto.dateOfBirth().isAfter(today)) {
             throw new IllegalArgumentException("Date of birth cannot be in the future");
         }
 
-        if (dto.getHireDate().isBefore(dto.getDateOfBirth())) {
+        if (dto.hireDate().isBefore(dto.dateOfBirth())) {
             throw new IllegalArgumentException("Hire date cannot be before date of birth");
         }
 
-        if (dto.getHireDate().isAfter(today)) {
+        if (dto.hireDate().isAfter(today)) {
             throw new IllegalArgumentException("Hire date cannot be in the future");
         }
 
-        if (DateUtils.calculatePeriod(dto.getDateOfBirth(), today).getYears() < 18) {
+        if (DateUtils.calculatePeriod(dto.dateOfBirth(), today).getYears() < 18) {
             throw new IllegalArgumentException("Employee must be at least 18 years old");
         }
     }
