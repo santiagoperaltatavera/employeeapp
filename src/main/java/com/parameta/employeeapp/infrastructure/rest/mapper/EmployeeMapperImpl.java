818bd90c0adfdffafbec2e178483ed07d2b103e6
santiagoperaltatavera <santiagoperalta95@hotmail.com>
2025-06-19 19:15:54 -0500

    Refactor EmployeeMapperImpl to improve mapping logic and support new DTO structure.

--- src/main/java/com/parameta/employeeapp/infrastructure/rest/mapper/EmployeeMapperImpl.java
+++ src/main/java/com/parameta/employeeapp/infrastructure/rest/mapper/EmployeeMapperImpl.java
@@ -2,31 +2,31 @@ package com.parameta.employeeapp.infrastructure.rest.mapper;
 
 import com.parameta.employeeapp.application.mappers.EmployeeMapper;
 import com.parameta.employeeapp.domain.model.Employee;
-import com.parameta.employeeapp.infrastructure.rest.dto.EmployeeRequestDTOImpl;
-import com.parameta.employeeapp.infrastructure.rest.dto.EmployeeResponseDTOImpl;
+import com.parameta.employeeapp.infrastructure.rest.dto.EmployeeRequestDTO;
+import com.parameta.employeeapp.infrastructure.rest.dto.EmployeeResponseDTO;
 import com.parameta.employeeapp.shared.utils.DateUtils;
 import org.springframework.stereotype.Component;
 
 @Component
-public class EmployeeMapperImpl implements EmployeeMapper<EmployeeRequestDTOImpl, EmployeeResponseDTOImpl> {
+public class EmployeeMapperImpl implements EmployeeMapper<EmployeeRequestDTO, EmployeeResponseDTO> {
 
     @Override
-    public Employee toEmployee(EmployeeRequestDTOImpl dto) {
+    public Employee toEmployee(EmployeeRequestDTO dto) {
         return Employee.builder()
-                .firstName(dto.getFirstName())
-                .lastName(dto.getLastName())
-                .documentType(dto.getDocumentType())
-                .documentNumber(dto.getDocumentNumber())
-                .dateOfBirth(dto.getDateOfBirth())
-                .hireDate(dto.getHireDate())
-                .position(dto.getPosition())
-                .salary(dto.getSalary())
+                .firstName(dto.firstName())
+                .lastName(dto.lastName())
+                .documentType(dto.documentType())
+                .documentNumber(dto.documentNumber())
+                .dateOfBirth(dto.dateOfBirth())
+                .hireDate(dto.hireDate())
+                .position(dto.position())
+                .salary(dto.salary())
                 .build();
     }
 
     @Override
-    public EmployeeResponseDTOImpl toResponseDTO(Employee employee, String age, String timeInCompany) {
-        return new EmployeeResponseDTOImpl(
+    public EmployeeResponseDTO toResponseDTO(Employee employee, String age, String timeInCompany) {
+        return new EmployeeResponseDTO(
                 employee.getFirstName(),
                 employee.getLastName(),
                 employee.getDocumentType(),
