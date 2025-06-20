f9331cae5db40ff3e531d053f4281d257d4a5fd2
santiagoperaltatavera <santiagoperalta95@hotmail.com>
2025-06-19 19:15:54 -0500

    Enhance EmployeeServiceImpl with additional business logic and improved interaction with ports.

--- src/main/java/com/parameta/employeeapp/application/services/EmployeeServiceImpl.java
+++ src/main/java/com/parameta/employeeapp/application/services/EmployeeServiceImpl.java
@@ -4,8 +4,8 @@ import com.parameta.employeeapp.application.mappers.EmployeeMapper;
 import com.parameta.employeeapp.application.ports.out.EmployeeSoapClientPort;
 import com.parameta.employeeapp.application.validators.EmployeeValidator;
 import com.parameta.employeeapp.domain.model.Employee;
-import com.parameta.employeeapp.infrastructure.rest.dto.EmployeeRequestDTOImpl;
-import com.parameta.employeeapp.infrastructure.rest.dto.EmployeeResponseDTOImpl;
+import com.parameta.employeeapp.infrastructure.rest.dto.EmployeeRequestDTO;
+import com.parameta.employeeapp.infrastructure.rest.dto.EmployeeResponseDTO;
 import com.parameta.employeeapp.shared.utils.DateUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,22 +13,22 @@ import org.springframework.transaction.annotation.Transactional;
 import java.time.LocalDate;
 
 @Service
-public class EmployeeServiceImpl implements EmployeeService<EmployeeRequestDTOImpl, EmployeeResponseDTOImpl> {
+public class EmployeeServiceImpl implements EmployeeService<EmployeeRequestDTO, EmployeeResponseDTO> {
 
     private final EmployeeSoapClientPort<Employee> employeeSoapClientPort;
-    private final EmployeeMapper<EmployeeRequestDTOImpl, EmployeeResponseDTOImpl> employeeMapper;
-    private final EmployeeValidator<EmployeeRequestDTOImpl> employeeValidator;
+    private final EmployeeMapper<EmployeeRequestDTO, EmployeeResponseDTO> employeeMapper;
+    private final EmployeeValidator<EmployeeRequestDTO> employeeValidator;
 
     public EmployeeServiceImpl(EmployeeSoapClientPort<Employee>  employeeSoapClientPort,
-                               EmployeeMapper<EmployeeRequestDTOImpl, EmployeeResponseDTOImpl> employeeMapper,
-                               EmployeeValidator<EmployeeRequestDTOImpl>  employeeValidator) {
+                               EmployeeMapper<EmployeeRequestDTO, EmployeeResponseDTO> employeeMapper,
+                               EmployeeValidator<EmployeeRequestDTO>  employeeValidator) {
         this.employeeSoapClientPort = employeeSoapClientPort;
         this.employeeMapper = employeeMapper;
         this.employeeValidator = employeeValidator;
     }
 
     @Transactional
-    public EmployeeResponseDTOImpl createEmployee(EmployeeRequestDTOImpl requestDTO) {
+    public EmployeeResponseDTO createEmployee(EmployeeRequestDTO requestDTO) {
         employeeValidator.validate(requestDTO);
 
         Employee employee = employeeMapper.toEmployee(requestDTO);
